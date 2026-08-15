af1eba0658e30e6ddfbd8e6951994e32dabab7b0
dsdengrasa08 <q98668aa@E-LOSX20HC6.local>
2026-08-15 15:14:44 +0100

    Updated files

--- pdf_layout_gradio_app/src/envira_layout_web/errors.py
+++
@@ -1,4 +0,0 @@
-"""User-safe web application errors."""
-
-class WebAppError(RuntimeError):
-    """An expected error whose message is safe to display."""
--- pdf_layout_gradio_app/src/envira_layout_web/ui/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Gradio presentation layer."""
