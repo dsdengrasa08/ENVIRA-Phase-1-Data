4382a4636ac63d9cf1aea69443cfa40780649eba
dsdengrasa08 <q98668aa@E-LOSX20HC6.local>
2026-09-05 15:14:00 +0100

    Updated files

--- pdf_layout_pipeline-AEE-vBckp/tests/test_region_conversion.py
+++
@@ -1,7 +0,0 @@
-from envira_pdf_layout.region_conversion import docling_label_to_region_type
-
-
-def test_label_mapping():
-    assert docling_label_to_region_type("section_header") == "Section-header"
-    assert docling_label_to_region_type("picture") == "Figure"
-    assert docling_label_to_region_type("new_label") == "Unknown"
